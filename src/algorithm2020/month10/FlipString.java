66a9a1644012c1cfffdbb46fc51e45f0a5e397a8
kanghangyul <gang6607@gmail.com>
2020-10-30 23:54:27 +0900

    2020 10 30 23:54 commit

--- src/algorithm2020/month10/FlipString.java
+++ src/algorithm2020/month10/FlipString.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 public class FlipString {
     private static String getFlipString(String str) {
         char[] charArr = new char[str.length()];
-        int cnt = charArr.length;
         String returnStr = "";
 
+        if(str.length() < 3) {
+            returnStr = str;
+            return returnStr;
+        }
+
+        int i;
+
         // char 1차원 배열에 받은 문자열 한 단어씩 넣기
-        for (int i = 0; i < cnt; i++) {
+        for (i = 0; i < str.length(); i++) {
             charArr[i] = str.charAt(i);
         }
 
         // 문자열을 뒤집어서 입력 받기
-        for (int i = 0; i < cnt; i++) {
-            returnStr += charArr[cnt -(i+1)];
+        for (i = 0; i < str.length(); i++) {
+            returnStr += charArr[str.length() -(i+1)];
         }
         return returnStr;
     }
@@ -25,7 +31,7 @@ public class FlipString {
         String str = scan.next();
 
         // 두 단어 이상 받을 수 있도록 조건 처리
-        if(str.length() < 1) {
+        if(str.length() < 2) {
             System.out.println("받은 문자열이 없습니다. 두 단어 이상 입력하세요.");
             String[] garbageStringArr = new String[0];
             main(garbageStringArr);
