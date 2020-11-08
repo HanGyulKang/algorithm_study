0322405c2577fe26478bbf2cd686598462d29c97
kanghangyul <gang6607@gmail.com>
2020-11-08 21:18:39 +0900

    2020 11 08 21:18 commit

---
+++ src/algorithm2020/month11/baek_15596.java
@@ -0,0 +1,28 @@
+package algorithm2020.month11;
+
+import java.util.Scanner;
+
+public class baek_15596 {
+
+    static long sum(int[] a) {
+        int total = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            total += a[i];
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = 10;
+
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        System.out.println(sum(a));
+    }
+}
