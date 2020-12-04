1800e3a01f3c5db30dfc9506374452e7c15d5708
kanghangyul <gang6607@gmail.com>
2020-12-04 22:06:34 +0900

    2020 12 04 22:06 commit

---
+++ src/algorithm2020/month12/baek_2839.java
@@ -0,0 +1,24 @@
+package algorithm2020.month12;
+
+public class baek_2839 {
+    public static void main(String[] args) {
+        int a = 21;
+        int i = 5;
+        int j = 3;
+        int res = 0;
+
+
+        if((a % i) - j == 0) {
+            res += a / i;
+            res += res / j;
+        } else if (a % i == 0) {
+            res += a / i;
+        } else if (a % j == 0){
+            res += a / j;
+        } else {
+            res = -1;
+        }
+        System.out.println(res);
+
+    }
+}
