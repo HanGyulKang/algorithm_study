cfda6089149b4a8abc1d1c5d87d9fcba9b694bbb
kanghangyul <gang6607@gmail.com>
2020-12-11 22:54:28 +0900

    2020 12 11 22:54 commit

--- src/algorithm2020/month12/baek_2751.java
+++ src/algorithm2020/month12/baek_2751.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 
 public class baek_2751 {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int N=sc.nextInt();
-        int [] arr =new int[N];
-        for(int i=0;i<N;i++) {
-            arr[i]=sc.nextInt();
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scan.nextInt();
         }
         Arrays.sort(arr);
-
-        for(int i=0;i<N;i++) {
+        for (int i = 0; i < N; i++) {
             System.out.println(arr[i]);
         }
     }
