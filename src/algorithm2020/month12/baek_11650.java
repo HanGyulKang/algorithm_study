ea7191728f96c2664b93f43f5087a01ad1c3b17b
kanghangyul <gang6607@gmail.com>
2020-12-26 23:25:55 +0900

    2020 12 26 23:25 commit

---
+++ src/algorithm2020/month12/baek_11650.java
@@ -0,0 +1,35 @@
+package algorithm2020.month12;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class baek_11650 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int size = scan.nextInt();
+        int[][] arr = new int[size][2];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = scan.nextInt();
+            }
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+                return o1[0] - o2[0];
+            }
+        });
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+
+    }
+}
