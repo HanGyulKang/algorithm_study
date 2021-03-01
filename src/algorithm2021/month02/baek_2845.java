6cfdf586d6edc8cd02f2f3a5ad6779f4d69b1ef3
kanghangyul <gang6607@gmail.com>
2021-03-01 23:39:40 +0900

    2021 03 01 23:39 commit

---
+++ src/algorithm2021/month02/baek_2845.java
@@ -0,0 +1,27 @@
+package algorithm2021.month02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class baek_2845 {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int partyMember = scan.nextInt();
+        int size = scan.nextInt();
+
+        int totalMember = partyMember * size;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] newspaper = new int[5];
+        String[] data = br.readLine().split(" ");
+        for (int i = 0; i < 5; i++) {
+            newspaper[i] = Integer.parseInt(data[i]);
+            System.out.print((newspaper[i] - totalMember) + " ");
+        }
+
+
+
+    }
+}
