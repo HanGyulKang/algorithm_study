a28d3140fd929e26858d948b86c8aa3b7bf90a4c
kanghangyul <gang6607@gmail.com>
2021-03-04 16:48:17 +0900

    2021 03 04 16:48 commit

---
+++ src/algorithm2021/month03/baek_8370.java
@@ -0,0 +1,27 @@
+package algorithm2021.month03;
+
+import java.io.*;
+
+public class baek_8370 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] strArr = (br.readLine()).split(" ");
+
+        int cnt = 2;
+        int total = 0;
+        // 받은 배열을 2개씩 끊어서 곱해준 값을 total에 누적
+        while(true) {
+            if(cnt > strArr.length) {
+                break;
+            } else {
+                total += (Integer.parseInt(strArr[cnt - 2]) * Integer.parseInt(strArr[cnt - 1]));
+                cnt += 2;
+            }
+        }
+
+        bw.write(String.valueOf(total));
+        bw.flush();
+        br.close();
+    }
+}
