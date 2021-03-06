effe6470ea3dd7a43b18a26a6a29933660f2a193
kanghangyul <gang6607@gmail.com>
2021-03-06 21:13:23 +0900

    2021 03 06 commit

--- src/algorithm2021/month03/baek_14928.java
+++ src/algorithm2021/month03/baek_14928.java
@@ -1,14 +1,25 @@
 package algorithm2021.month03;
 
-import java.math.BigInteger;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class baek_14928 {
     final static int NUM = 20000303;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        BigInteger a = scan.nextBigInteger();
-        System.out.println(a.mod(BigInteger.valueOf(NUM)));
+        String str = scan.next();
+
+        int a = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            int num = str.charAt(i)-'0';
+            a *= 10;
+            a += num;
+            a %= NUM;
+            System.out.println(num + " / " + a);
+
+        }
+        System.out.println(a);
     }
 }
