16eb1d3e6be07a129fa6d024ebb925106c546fb2
kanghangyul <gang6607@gmail.com>
2021-03-06 20:44:04 +0900

    2021 03 05 commit

--- src/algorithm2021/month03/baek_11283.java
+++ src/algorithm2021/month03/baek_11283.java
@@ -4,29 +4,9 @@ import java.util.Scanner;
 
 public class baek_11283 {
     public static void main(String[] args) {
-        // 19, 21, 28
-
         Scanner scan = new Scanner(System.in);
-        String str = scan.next();
-        char c = str.charAt(0);
-
-        // 초성 공식
-        // ((문자유니코드 - 0xAC00)/28)/21
-        char f = (char)(((c - 0xAC00) / 28) / 21);
-
-        // 중성 공식
-        // (문자유니코드 - 0xAC00) / 28 % 21
-        char m = (char)(((c - 0xAC00) / 28) % 21);
-
-        // 종성 공식
-        // (문자유니코드 - 0xAC00) % 28
-        char b = (char)((c - 0xAC00) % 28);
-
-        int A = f;
-        int B = m;
-        int C = b == 0 ? 1 : b + 1;
-        System.out.println();
-
+        int c = scan.next().charAt(0);
+        System.out.println(c - 44031);
     }
 }
 
