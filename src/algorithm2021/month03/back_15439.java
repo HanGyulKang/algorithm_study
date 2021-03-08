d214087e72d8c238e6199b5084cf305401a18472
kanghangyul <gang6607@gmail.com>
2021-03-08 23:20:19 +0900

    2021 03 08 commit

---
+++ src/algorithm2021/month03/back_15439.java
@@ -0,0 +1,11 @@
+package algorithm2021.month03;
+
+import java.util.Scanner;
+
+public class back_15439 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        System.out.println(N * (N - 1));
+    }
+}
