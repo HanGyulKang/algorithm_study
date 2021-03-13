e6f6e93159e2f03dca8e9566d5340e40719074e2
kanghangyul <gang6607@gmail.com>
2021-03-13 23:30:43 +0900

    2021 03 13 commit

---
+++ src/algorithm2021/month03/baek_17496.java
@@ -0,0 +1,26 @@
+package algorithm2021.month03;
+
+import java.util.Scanner;
+
+public class baek_17496 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int summer = scan.nextInt();
+        int day = scan.nextInt();
+        int cnt = scan.nextInt();
+        int price = scan.nextInt();
+
+        int res = 1;
+        int resPrice = 0;
+
+        do {
+            res += day;
+            if(summer < res) {
+                break;
+            }
+            resPrice += cnt * price;
+        } while(true);
+
+        System.out.println(resPrice);
+    }
+}
