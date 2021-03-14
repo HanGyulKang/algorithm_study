1c7f779455cc1ce0d709ef9159ee17093c754d29
kanghangyul <gang6607@gmail.com>
2021-03-14 23:38:55 +0900

    2021 03 14 commit

---
+++ src/algorithm2021/month03/back_2525.java
@@ -0,0 +1,25 @@
+package algorithm2021.month03;
+
+import java.util.Scanner;
+
+public class back_2525 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int h = scan.nextInt();
+        int m = scan.nextInt();
+        int time = scan.nextInt();
+
+        m += time;
+        if(m >= 60) {
+            while(m >= 60) {
+                m -= 60;
+                h++;
+                if(h >= 24) {
+                    h = 0;
+                }
+            }
+        }
+
+        System.out.println(h + " " + m);
+    }
+}
