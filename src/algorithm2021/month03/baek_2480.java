0bfca5ac9b48c13470b7868c25f3796c37d5e6d0
kanghangyul <gang6607@gmail.com>
2021-03-14 23:38:55 +0900

    2021 03 14 commit

---
+++ src/algorithm2021/month03/baek_2480.java
@@ -0,0 +1,31 @@
+package algorithm2021.month03;
+
+import java.util.Scanner;
+
+public class baek_2480 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        if(a == b && b == c && a == c) {
+            System.out.println(10000 + (a * 1000));
+        } else if(a == b) {
+            System.out.println(1000 + (a * 100));
+        } else if(a == c) {
+            System.out.println(1000 + (a * 100));
+        } else if(b == c) {
+            System.out.println(1000 + (b * 100));
+        } else {
+            int max = a;
+            if(b > max) {
+                max = b;
+            }
+            if(c > max) {
+                max = c;
+            }
+            System.out.println(max * 100);
+        }
+    }
+}
