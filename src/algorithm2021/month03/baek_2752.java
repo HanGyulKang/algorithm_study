8171d4dd043e4198fae3e60c51811982b5bc5c9a
kanghangyul <gang6607@gmail.com>
2021-03-16 22:56:07 +0900

    2021 03 16 commit

---
+++ src/algorithm2021/month03/baek_2752.java
@@ -0,0 +1,29 @@
+package algorithm2021.month03;
+
+import java.util.Scanner;
+
+public class baek_2752 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        int max = a;
+        if(b > max) {
+            max = b;
+        }
+        if(c > max) {
+            max = c;
+        }
+
+        int min = a;
+        if(b < min) {
+            min = b;
+        }
+        if(c < min) {
+            min = c;
+        }
+        System.out.print(min + " " + ((a + b + c) - max - min) + " " +  max);
+    }
+}
