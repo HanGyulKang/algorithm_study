882d121f3cfc5d28be2eed64a99ff0c9dd3603e5
kanghangyul <gang6607@gmail.com>
2021-03-17 23:34:34 +0900

    2021 03 17 commit

---
+++ src/algorithm2021/month03/baek_4299.java
@@ -0,0 +1,22 @@
+package algorithm2021.month03;
+
+import java.util.Scanner;
+
+public class baek_4299 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sum = scan.nextInt();
+        int diff = scan.nextInt();
+
+        if(sum - diff < 0) {
+            System.out.println(-1);
+        } else {
+            if((sum - diff) % 2 == 0) {
+                System.out.println((sum - diff) / 2 + diff + " " + (sum - diff) / 2);
+            } else {
+                System.out.println(-1);
+            }
+        }
+        scan.close();
+    }
+}
