deaa0c258f3019ea4b82eb33dcb9ba7da965e48a
kanghangyul <gang6607@gmail.com>
2021-03-22 23:20:50 +0900

    2021 03 22 commit

---
+++ src/algorithm2021/month03/baek_5893.java
@@ -0,0 +1,19 @@
+package algorithm2021.month03;
+
+public class baek_5893 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("10110111").reverse();
+        String str = sb.toString();
+
+        int num = 1;
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if((str.charAt(i) - '0') == 1) {
+                sum += num;
+            }
+            num *= 2;
+        }
+        System.out.println(sum * 17);
+    }
+}
