60d0eb48d997fe174e20d80a0cc4cb27b5dc71b9
kanghangyul <gang6607@gmail.com>
2021-05-06 23:22:57 +0900

    2021 05 06 commit

--- src/algorithm2021/month05/baek_11948.java
+++ src/algorithm2021/month05/baek_11948.java
@@ -22,6 +22,7 @@ public class baek_11948 {
     }
 
     public static void arraySumUtil(int[] arr) {
+        // 가장 작은 점수는 누락시킨다.
         for (int i = 0; i < arr.length - 1; i++) {
             sum += arr[i];
         }
@@ -33,22 +34,28 @@ public class baek_11948 {
 
         Scanner scan = new Scanner(System.in);
 
+        // 시험 유형 A의 점수를 받는다
         for (int i = 0; i < scoreA.length; i++) {
             scoreA[i] = scan.nextInt();
         }
 
+        // 시험 유형 B의 점수를 받는다
         for (int i = 0; i < scoreB.length; i++) {
             scoreB[i] = scan.nextInt();
         }
 
+        // 정렬 유틸 메소드를 호출하여 배열을 큰 수 부터 정렬한다.
         scoreA = arraySortUtil(scoreA);
         scoreB = arraySortUtil(scoreB);
 
+        // 가장 작은 값 하나를 제외한 나머지 값을 전역변수에 더하는 메소드를 호출한다.
         arraySumUtil(scoreA);
         arraySumUtil(scoreB);
 
+        // 더해진 값을 출력한다.
         System.out.println(sum);
 
+        // 다시 0으로 초기화한다.
         sum = 0;
     }
 }
