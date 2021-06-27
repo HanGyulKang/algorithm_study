4e23273e31ad2e52f1be963f7bfae8dd7deab12d
kanghangyul <gang6607@gmail.com>
2021-06-28 00:08:12 +0900

    2021 06 28 commit

---
+++ src/algorithm2021/month06/baek_15873.java
@@ -0,0 +1,24 @@
+package algorithm2021.month06;
+
+import java.util.Scanner;
+
+public class baek_15873 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int res = 0;
+
+        System.out.println(109 / 10);
+
+        if(a / 10 < 10) {
+            res = (a / 10) + (a % 10);
+        } else if(a / 10 == 10) {
+            res = (a / 10) + (a % 10);
+        } else {
+            res = 20;
+        }
+
+        System.out.println(res);
+    }
+}
