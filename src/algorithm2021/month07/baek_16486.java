41e0238a1a905147cff0aa138c86e41b86351925
kanghangyul <gang6607@gmail.com>
2021-07-05 23:44:26 +0900

    2021 07 05 commit

---
+++ src/algorithm2021/month07/baek_16486.java
@@ -0,0 +1,15 @@
+package algorithm2021.month07;
+
+import java.util.Scanner;
+
+public class baek_16486 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double A = scan.nextInt();
+        double B = scan.nextInt();
+
+        double res = (A * 2) + (2 * 3.141592 * B);
+        System.out.println(res);
+    }
+}
