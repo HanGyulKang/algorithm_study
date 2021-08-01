625de7567019837bdc8d78a3d8c2390fe4a23482
kanghangyul <gang6607@gmail.com>
2021-08-01 22:27:45 +0900

    2021 08 01

--- src/InflearnJavaClass2021/Main002.java
+++ src/InflearnJavaClass2021/Main002.java
@@ -3,25 +3,25 @@ package InflearnJavaClass2021;
 import java.util.Scanner;
 
 public class Main002 {
-    public String solution(String str) {
+    public String solution(String str, boolean flag) {
         String res = "";
 
         // 1. for each 풀이
         for (char c : str.toCharArray()) {
-            // 1. lowercase, uppercase로 풀이
-            /*
-            if(Character.isLowerCase(c)) {
-                res += Character.toUpperCase(c);
+            if(flag) {
+                // 1. lowercase, uppercase로 풀이
+                if(Character.isLowerCase(c)) {
+                    res += Character.toUpperCase(c);
+                } else {
+                    res += Character.toLowerCase(c);
+                }
             } else {
-                res += Character.toLowerCase(c);
-            }
-            */
-
-            // 2. ASCII 코드로 풀이
-            if(c >= 65 && c <= 90) { // 대문자
-                res += (char)(c + 32);
-            } else if(c >= 97 && c <= 122) {
-                res += (char)(c - 32);
+                // 2. ASCII 코드로 풀이
+                if(c >= 65 && c <= 90) { // 대문자
+                    res += (char)(c + 32);
+                } else if(c >= 97 && c <= 122) {
+                    res += (char)(c - 32);
+                }
             }
         }
 
@@ -34,6 +34,6 @@ public class Main002 {
         Scanner scan = new Scanner(System.in);
         String str = scan.next();
 
-        System.out.println(M002.solution(str));
+        System.out.println(M002.solution(str, true));
     }
 }
