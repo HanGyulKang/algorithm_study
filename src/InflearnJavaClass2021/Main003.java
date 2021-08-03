4c120de1f55ec1e86f0d4c4adbf29b38a32f2413
kanghangyul <gang6607@gmail.com>
2021-08-03 23:45:24 +0900

    2021 08 03

--- src/InflearnJavaClass2021/Main003.java
+++ src/InflearnJavaClass2021/Main003.java
@@ -2,15 +2,20 @@ package InflearnJavaClass2021;
 
 import java.io.*;
 
-public class Main {
+public class Main003 {
     public static String solution(String str) {
         String[] strArr = str.split(" ");
+        String strRes = "";
+        int temp = 0;
 
         for(String s : strArr) {
-            System.out.println(s);
+            if(temp < s.length()) {
+                strRes = s;
+                temp = s.length();
+            }
         }
 
-        return "";
+        return strRes;
     }
 
     public static void main(String[] args) throws IOException {
