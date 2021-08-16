bcd707d25e2b04523bfbaf6ca086ba156592f898
kanghangyul <gang6607@gmail.com>
2021-08-16 23:48:31 +0900

    2021 08 16

--- src/InflearnJavaClass2021/Main005_self.java
+++ src/InflearnJavaClass2021/Main005_self.java
@@ -9,31 +9,20 @@ public class Main005_self {
         int front = 0;
         int back = str.length() - 1;
 
-        // 소문자 = 97 ~ 122, 대문자 = 65 ~ 90
         char[] c = str.toCharArray();
 
         while(front < back) {
-            char tmp = 0;
-            int frontChar = c[front];
-            int backChar = c[back];
-
-            int cnt = 0;
-
-            if((frontChar >= 97 && frontChar <= 122) || (frontChar >= 65 && frontChar <= 90)) {
-                cnt++;
-            }
-            if((backChar >= 97 && backChar <= 122) || (backChar >= 65 && backChar <= 90)) {
-                cnt++;
-            }
-
-            if(cnt == 2) {
-                tmp = c[front];
+            if(!Character.isAlphabetic(c[front])) {
+                front++;
+            } else if(!Character.isAlphabetic(c[back])) {
+                back--;
+            } else {
+                char temp = c[front];
                 c[front] = c[back];
-                c[back] = tmp;
+                c[back] = temp;
+                front++;
+                back--;
             }
-
-            front++;
-            back--;
         }
 
         answer = String.valueOf(c);
---
+++ src/InflearnJavaClass2021/Main006.java
@@ -0,0 +1,26 @@
+package InflearnJavaClass2021;
+
+import java.util.Scanner;
+
+public class Main006 {
+    private static String solution(String str) {
+        String answer = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            //System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+
+            if(str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+
+        System.out.println(solution(str));
+    }
+}
