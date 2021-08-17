5d852f8d830f8fa6aee30ba60308c25a3746c189
kanghangyul <gang6607@gmail.com>
2021-08-18 01:24:49 +0900

    2021 08 18

---
+++ src/InflearnJavaClass2021/Main007.java
@@ -0,0 +1,31 @@
+package InflearnJavaClass2021;
+
+import java.util.Scanner;
+
+public class Main007 {
+    private String solution_01(String str) {
+        String answer = "YES";
+        char[] s = str.toCharArray();
+
+        int lt = 0;
+        int rt = str.length() - 1;
+
+        while(lt < rt) {
+            if(s[lt] != s[rt]) {
+                answer = "NO";
+            }
+
+            lt++;
+            rt--;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next().toUpperCase();
+
+        System.out.println(new Main007().solution_01(str));
+    }
+}
