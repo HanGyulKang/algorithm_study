0fb53bfe475006c2cf4f358204e3f7532eba87be
kanghangyul <gang6607@gmail.com>
2021-08-29 20:14:08 +0900

    2021 08 29

---
+++ src/InflearnJavaClass2021/Main009_teach.java
@@ -0,0 +1,38 @@
+package InflearnJavaClass2021;
+
+import java.util.Scanner;
+
+public class Main009_teach {
+    public int solution_02(String str) {
+        String answer = "";
+
+        for(char x : str.toCharArray()) {
+            if(Character.isDigit(x)) {
+                answer += x;
+            }
+        }
+
+        return Integer.parseInt(answer);
+    }
+
+    public int solution_01(String str) {
+        int answer = 0;
+
+        for (char x : str.toCharArray()) {
+            if(x >= 48 && x <= 57) {
+                answer = answer * 10 + (x - 48);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main009_teach T = new Main009_teach();
+
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        System.out.println(T.solution_01(str));
+        System.out.println(T.solution_02(str));
+    }
+}
