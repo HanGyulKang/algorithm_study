4b58aaaf053066fdc7117ca0f68eefc267d734c4
kanghangyul <gang6607@gmail.com>
2021-08-30 23:10:57 +0900

    2021 08 30

--- src/InflearnJavaClass2021/Main011.java
+++ src/InflearnJavaClass2021/Main011.java
@@ -3,33 +3,37 @@ package InflearnJavaClass2021;
 import java.util.Scanner;
 
 public class Main011 {
-    public String solution(String str) {
-       String answer = "";
-        str = str + " ";
+    public Main011() {
+        // System.out.println("Main011 생성자 호출");
+    }
+
+    private String solution(String s) {
+        String answer = "";
+        s += " ";
         int cnt = 1;
 
-        for(int i = 0; i < str.length() - 1; i++) {
-            if(str.charAt(i) == str.charAt(i + 1)) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if(s.charAt(i) == s.charAt(i + 1)) {
                 cnt++;
             } else {
-                answer += str.charAt(i);
+                answer += s.charAt(i);
 
                 if(cnt > 1) {
-                    //answer += String.valueOf(cnt);
                     answer += Integer.toString(cnt);
                     cnt = 1;
                 }
             }
         }
 
-       return answer;
+        return answer;
     }
 
     public static void main(String[] args) {
-        Main011 T = new Main011();
         Scanner scan = new Scanner(System.in);
-
         String str = scan.next();
-        System.out.println(T.solution(str));
+
+        Main011 Obj = new Main011();
+        System.out.println(Obj.solution(str));
+
     }
 }
