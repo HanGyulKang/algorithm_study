c98fec05170ee39ffe8c40e5e02e0de005f628e8
kanghangyul <gang6607@gmail.com>
2021-08-30 23:30:45 +0900

    2021 08 30

---
+++ src/InflearnJavaClass2021/Main.java
@@ -0,0 +1,34 @@
+package InflearnJavaClass2021;
+
+import java.util.Scanner;
+
+public class Main {
+    private static final int DIVNUM = 7;
+
+    private String solution(String str) {
+        String answer = "";
+
+        str = str.replace("#", "1");
+        str = str.replace("*", "0");
+
+        String tmp = "";
+        for (int i = 0; i < str.length(); i++) {
+            if((i + 1) % DIVNUM == 0) {
+                tmp = str.substring(i + 1 - DIVNUM, i + 1);
+                answer += (char)Integer.parseInt(tmp, 2);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+
+        Main Obj = new Main();
+        System.out.println(Obj.solution(str));
+
+        scan.close();
+    }
+}
