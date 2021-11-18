4c43830952e22aae0866eda625763c2f93067a2a
kanghangyul <gang6607@gmail.com>
2021-11-18 12:33:47 +0900

    2021 11 18

--- src/InflearnJavaClass2021/class02_Array/Main016_teach.java
+++ src/InflearnJavaClass2021/class02_Array/Main016_teach.java
@@ -6,10 +6,9 @@ public class Main016_teach {
     public void solution_02(int n) {
         int a = 1, b = 1, c;
 
-        System.out.print(a + " " + b + " ");
         for (int i = 2; i < n; i++) {
             c = a + b;
-            System.out.print(c + " ");
+
             a = b;
             b = c;
         }
