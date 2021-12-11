cb31a14632c43305dc4ae130a2128dc4696e7ad7
kanghangyul <gang6607@gmail.com>
2021-12-11 17:26:20 +0900

    2021 12 11

--- src/YouRa_Homework.java
+++ src/YouRa_Homework.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DailyTraining {
+public class YouRa_Homework {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = 10;
