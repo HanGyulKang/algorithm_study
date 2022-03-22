3e38fd0b74a414fc38c68c04d21a0ce26d22086d
kanghangyul <gang6607@gmail.com>
2022-03-23 00:18:50 +0900

    2022 03 27

--- src/InflearnJavaClass2022/class02_Array/Main0209_review.java
+++ src/InflearnJavaClass2022/class02_Array/Main0209_review.java
@@ -6,19 +6,21 @@ import java.util.Scanner;
 
 public class Main0209_review {
     private int getMaxNum(ArrayList<Integer> arrList) {
+        // 리스트로 받은 숫자들을 Sorting 함
         Collections.sort(arrList);
+        // 오름차순으로 정렬되었으니 배열 마지막 index의 값이 가장 큰 수
         return arrList.get(arrList.size() - 1);
     }
 
     private int solution(int[][] arr, int n) {
-        int answer = 0;
-        int k = n - 1;
-
         ArrayList<Integer> arrayList = new ArrayList<>();
-        int sum_row;
-        int sum_col;
-        int sum_RtoL = 0;
-        int sum_LtoR = 0;
+
+        int answer = 0;
+        // 우 -> 좌 대각선 값을 구하기 위한 j좌표 값 초기 셋팅
+        int k = n - 1;
+        // 행, 열, 대각선 합계를 담을 변수
+        int sum_row, sum_col;
+        int sum_RtoL = 0, sum_LtoR = 0;
 
         // 행, 열, 대각선 값
         for (int i = 0; i < n; i++) {
@@ -37,14 +39,17 @@ public class Main0209_review {
                 // 열
                 sum_col += arr[j][i];
             }
+            // 가장 큰 값을 구하기 위해 계산되는 행과 열의 합을 List에 넣음
             arrayList.add(sum_row);
             arrayList.add(sum_col);
 
         }
 
+        // 대각선 합을 List에 마저 넣음
         arrayList.add(sum_LtoR);
         arrayList.add(sum_RtoL);
 
+        // 만들어둔 가장 큰 값을 찾는 메소드 호출
         answer = this.getMaxNum(arrayList);
 
         return answer;
