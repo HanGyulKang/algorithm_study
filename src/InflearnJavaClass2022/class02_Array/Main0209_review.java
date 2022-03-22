d26ec80473d7f3aaf1a45f138a00e876406e0a04
kanghangyul <gang6607@gmail.com>
2022-03-23 00:15:39 +0900

    2022 03 23

--- src/InflearnJavaClass2022/class02_Array/Main0209_review.java
+++ src/InflearnJavaClass2022/class02_Array/Main0209_review.java
@@ -1,27 +1,32 @@
 package InflearnJavaClass2022.class02_Array;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main0209_review {
+    private int getMaxNum(ArrayList<Integer> arrList) {
+        Collections.sort(arrList);
+        return arrList.get(arrList.size() - 1);
+    }
+
     private int solution(int[][] arr, int n) {
         int answer = 0;
-        int max = Integer.MIN_VALUE;
         int k = n - 1;
-        int sum_row = 0;
-        int sum_col = 0;
 
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int sum_row;
+        int sum_col;
+        int sum_RtoL = 0;
+        int sum_LtoR = 0;
+
+        // 행, 열, 대각선 값
         for (int i = 0; i < n; i++) {
             // 좌 -> 우 대각선 값
-            max += arr[i][i];
+            sum_LtoR += arr[i][i];
             // 우 -> 좌 대각선 값
-            sum_row += arr[i][k--];
-        }
+            sum_RtoL += arr[i][k--];
 
-        // 각 대각선의 합 중 가장 큰 값
-        max = Math.max(max, sum_row);
-
-        // 행, 열 값
-        for (int i = 0; i < n; i++) {
             // 각 행 열 계산이 끝날 때 마다 0으로 다시 초기화
             sum_row = 0;
             sum_col = 0;
@@ -32,12 +37,16 @@ public class Main0209_review {
                 // 열
                 sum_col += arr[j][i];
             }
-            sum_row = Math.max(sum_row, sum_col);
+            arrayList.add(sum_row);
+            arrayList.add(sum_col);
+
         }
 
-        max = Math.max(sum_row, max);
+        arrayList.add(sum_LtoR);
+        arrayList.add(sum_RtoL);
 
-        answer = max;
+        answer = this.getMaxNum(arrayList);
+
         return answer;
     }
 
