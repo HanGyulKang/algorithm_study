a9bd8684ae1a5099403622f97602ec97391c9806
kanghangyul <gang6607@gmail.com>
2022-03-29 22:52:35 +0900

    2022 03 29

--- src/DailyPractice.java
+++ src/DailyPractice.java
@@ -1,13 +1,64 @@
-import java.util.Scanner;
-
 public class DailyPractice {
+    // 심심할 때 찍는 별
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+        for (int i = 0; i < 5; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < i + 1; j++) {
-                System.out.print("*");
+        System.out.println();
+
+        //     *
+        //    **
+        //   ***
+        //  ****
+        // *****
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j >= 0; j--) {
+                if(j <= i) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // *****
+        //  ****
+        //   ***
+        //    **
+        //     *
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(j >= i) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // *****
+        // ****
+        // ***
+        // **
+        // *
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5 - i; j++) {
+                System.out.print('*');
             }
             System.out.println();
         }
