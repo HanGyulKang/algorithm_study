444fa6fdfc19b5bd7ba82f9e1ace19bb3e177887
kanghangyul <gang6607@gmail.com>
2022-03-31 12:44:18 +0900

    2022 03 31

--- src/util/ArrayUtil.java
+++ src/util/ArrayUtil.java
@@ -3,7 +3,7 @@ package util;
 import java.util.Scanner;
 
 public class ArrayUtil {
-    public static Scanner scan = new Scanner(System.in);
+    public Scanner scan = new Scanner(System.in);
 
     public void valueToArray(int n, int[] arr) {
         for(int i = 0; i < n; i++) {
