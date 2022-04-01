19d7e44b9a7de4d7b8e71b7863f50d96c46acecb
kanghangyul <gang6607@gmail.com>
2022-04-02 00:24:38 +0900

    2022 04 02

--- src/InflearnJavaClass2022/class04_Hash/Main0403_self.java
+++ src/InflearnJavaClass2022/class04_Hash/Main0403_self.java
@@ -13,17 +13,33 @@ public class Main0403_self {
         // n   : 장사 일 수
         // k   : 구간 일 수
         // arr : 각 일별 매출액
-        int p1 = 0;
-        int p2 = k;
+        // 1. 초기 값을 잡아준다.
+        for(int i = 0; i < k; i++) {
+            hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0) + 1);
+        }
+        answer.add(hashMap.size());
 
-        while(p2 <= n) {
-            for(int i = p1; i < p2; i++) {
-                hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0) + 1);
+        // 2. 초기값 + 1일부터 two pointer 알고리즘으로 푼다.
+        for(int i = k; i < n; i++) {
+            // rt값을 hashMap에 넣어준다.
+            // 이미 있는 값이면 Value만 1 증가하고 없는 값이면 1을 셋팅한다.
+            hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0) + 1);
+
+            // hashMap이 lt값을 가지고있는지 판단한다.
+            if(hashMap.containsKey(arr[i - k])) {
+                // 가지고있을 경우 Value를 1 감소시킨다.
+                hashMap.replace(arr[i - k], hashMap.get(arr[i- k]) - 1);
             }
+
+            // lt값에 해당하는(매출종류) Key의 value가 0이라면
+            // 즉 전진하는 구간 사이에서 매출종류가 사라졌다면
+            // hashMap에서 제거한다.
+            if(hashMap.get(arr[i - k]) == 0) {
+                hashMap.remove(arr[i - k]);
+            }
+
+            // 최종 결과를 ArrayList에 담는다.
             answer.add(hashMap.size());
-            hashMap.clear();
-            p1++;
-            p2++;
         }
 
         return answer;
---
+++ src/InflearnJavaClass2022/class04_Hash/Main0403_teach.java
@@ -0,0 +1,52 @@
+package InflearnJavaClass2022.class04_Hash;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main0403_teach {
+    private ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> HM = new HashMap<>();
+
+        // k - 1일째까지만 담는 것으로 초기화
+        for(int i = 0; i < k - 1; i++) {
+            HM.put(arr[i], HM.getOrDefault(arr[i], 0) + 1);
+        }
+
+        int lt = 0;
+        for(int rt = k - 1; rt < n; rt++) {
+            // 부족분이었던 마지막 하루분을 마저 담고
+            HM.put(arr[rt], HM.getOrDefault(arr[rt], 0) + 1);
+            // 사이즈를 answer에 담는다.
+            answer.add(HM.size());
+            // 왼쪽 포인터의 값은 사라지는 것이기 때문에 -1
+            HM.put(arr[lt], HM.get(lt) - 1);
+
+            // 삭제 대상인 lt key의 value가 0이라면
+            if(HM.get(arr[lt]) == 0) {
+                // HashMap에서 제거
+                HM.remove(lt);
+            }
+            lt++;
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        for(int x : new Main0403_teach().solution(n, k, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
