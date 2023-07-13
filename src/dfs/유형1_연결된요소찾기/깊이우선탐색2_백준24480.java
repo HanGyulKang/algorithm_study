c1052bf13ad7b4a2000f240f5b91bf48706e8cc1
HanGyulKang <gang6607@gmail.com>
2023-07-13 23:43:48 +0900

    백준24479

--- src/dfs/유형1_연결된요소찾기/깊이우선탐색2_백준24480.java
+++ src/dfs/유형1_연결된요소찾기/깊이우선탐색2_백준24480.java
@@ -5,14 +5,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class 깊이우선탐색1_백준24479 {
-
+public class 깊이우선탐색2_백준24480 {
     /**
      * [문제 링크]
-     * https://www.acmicpc.net/problem/24479
+     * https://www.acmicpc.net/problem/24480
      */
-
-    final static int MAX = 1000000 + 10; // 10은 여유분
+    final static int MAX = 100000 + 10;
     static ArrayList<Integer>[] graph;
     static boolean[] visited;
     static int N, M, R;
@@ -24,8 +22,7 @@ public class 깊이우선탐색1_백준24479 {
         answer[idx] = order;
         order++;
 
-        // 각 ArrayList마다 하나씩 뒤져보겠다.
-        for (int i = 0; i < graph[idx].size(); i++) {
+        for(int i = 0; i < graph[idx].size(); i++) {
             int nextNode = graph[idx].get(i);
             if(!visited[nextNode]) {
                 dfs(nextNode);
@@ -34,7 +31,6 @@ public class 깊이우선탐색1_백준24479 {
     }
 
     public static void main(String[] args) throws IOException {
-        // 0. 입력 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -43,18 +39,16 @@ public class 깊이우선탐색1_백준24479 {
         M = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
 
-        // 1. graph에 정보 채우기
         graph = new ArrayList[MAX];
-        for (int i = 0; i < N; i++) {
+        for(int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
 
         visited = new boolean[MAX];
         answer = new int[MAX];
-        // 방문한 순서
         order = 1;
 
-        for (int i = 0; i < M; i++) {
+        for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
@@ -62,16 +56,13 @@ public class 깊이우선탐색1_백준24479 {
             graph[y].add(x);
         }
 
-        // 2. 오름차순 정렬
-        for (int i = 0; i < N; i++) {
-            Collections.sort(graph[i]);
+        for (int i = 1; i <= N; i++) {
+            // 내림차순
+            Collections.sort(graph[i], Collections.reverseOrder());
         }
 
-
-        // 3. dfs(재귀함수 호출)
         dfs(R);
 
-        // 4. 출력
         for(int i = 1; i <= N; i++) {
             bw.write(String.valueOf(answer[i]));
             bw.newLine();
