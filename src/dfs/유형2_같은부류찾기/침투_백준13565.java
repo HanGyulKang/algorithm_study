3120d6448bc626e7f842d53d3795907e25d7aed2
HanGyulKang <gang6607@gmail.com>
2023-07-26 00:25:40 +0900

    침투

--- src/dfs/유형2_같은부류찾기/침투_백준13565.java
+++ src/dfs/유형2_같은부류찾기/침투_백준13565.java
@@ -1,5 +1,8 @@
 package dfs.유형2_같은부류찾기;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class 침투_백준13565 {
     /**
      * [문제 링크]
@@ -10,10 +13,57 @@ public class 침투_백준13565 {
     static boolean visited[][];
     static int dirY[] = {-1, 1, 0, 0};
     static int dirX[] = {0, 0, -1, 1};
-    static int T, M, K;
-    static boolean answer;
+    static int N, M, K;
+    static boolean answer = false;
 
-    public static void main(String[] args) {
+    static void dfs(int y, int x) {
+        if(y == N) {
+            answer = true;
+            return;
+        }
 
+        visited[y][x] = true;
+        for(int i = 0; i < 4; i++) {
+            int newY = y + dirY[i];
+            int newX = x + dirX[i];
+
+            if(map[newY][newX] && !visited[newY][newX]) {
+                dfs(newY, newX);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new boolean[MAX][MAX];
+        visited = new boolean[MAX][MAX];
+
+        for (int i = 1; i <= N; i++) {
+            String str = br.readLine();
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = str.charAt(j - 1) == '0' ? true : false;
+            }
+        }
+
+        for (int i = 1; i <= M; i++) {
+            if(map[1][i]) {
+                dfs(1, i);
+            }
+        }
+
+        if(answer) {
+            bw.write("YES");
+        } else {
+            bw.write("NO");
+        }
+
+        br.close();
+        bw.close();
     }
 }
