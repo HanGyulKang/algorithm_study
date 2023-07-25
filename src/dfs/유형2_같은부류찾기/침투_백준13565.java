2a845bb5f30858b3497ed1ff978cf4c912ef88dd
HanGyulKang <gang6607@gmail.com>
2023-07-25 23:24:03 +0900

    ì¹투

---
+++ src/dfs/유형2_같은부류찾기/침투_백준13565.java
@@ -0,0 +1,19 @@
+package dfs.유형2_같은부류찾기;
+
+public class 침투_백준13565 {
+    /**
+     * [문제 링크]
+     * https://www.acmicpc.net/problem/13565
+     */
+    final static int MAX = 1000 + 10;
+    static boolean map[][];
+    static boolean visited[][];
+    static int dirY[] = {-1, 1, 0, 0};
+    static int dirX[] = {0, 0, -1, 1};
+    static int T, M, K;
+    static boolean answer;
+
+    public static void main(String[] args) {
+
+    }
+}
