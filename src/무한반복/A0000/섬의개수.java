15132393a5605c8bdb361d70634660606246c491
HanGyulKang <gang6607@gmail.com>
2023-07-27 00:33:41 +0900

    섬의개수

--- src/무한반복/A0000/섬의개수.java
+++ src/무한반복/A0000/섬의개수.java
@@ -1,28 +1,28 @@
-package dfs.유형2_같은부류찾기;
+package 무한반복.A0000;
 
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class 섬의개수_백준4963 {
+public class 섬의개수 {
     /**
      * [문제 링크]
      * https://www.acmicpc.net/problem/4963
      */
-    final static int MAX = 50 + 10;
-    static boolean map[][];
-    static boolean visited[][];
+    static boolean[][] graph;
     static int dirY[] = {-1, -1, 0, 1, 1, 1, 0, -1};
     static int dirX[] = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int answer;
+    static int MAX = 50 + 10;
     static int N, M;
 
     static void dfs(int y, int x) {
-        visited[y][x] = true;
+        graph[y][x] = false;
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < dirX.length; i++) {
             int newY = y + dirY[i];
             int newX = x + dirX[i];
 
-            if (map[newY][newX] && ! visited[newY][newX]) {
+            if(graph[newY][newX]) {
                 dfs(newY, newX);
             }
         }
@@ -33,38 +33,35 @@ public class 섬의개수_백준4963 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         while(true) {
+            // 변수 선언
             StringTokenizer st = new StringTokenizer(br.readLine());
-
-            M = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken()); // 행 개수
+            N = Integer.parseInt(st.nextToken()); // 각 열의 요소 개수
 
             if(N == 0 && M == 0) {
                 break;
             }
 
-            map = new boolean[MAX][MAX];
-            visited = new boolean[MAX][MAX];
-            
-            // 1. map 정보 반영
-            for (int i = 1; i <= N; i++) {
+            // 그래프 초기화 및 데이터 입력
+            graph = new boolean[MAX][MAX];
+            for(int i = 1; i <= N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 1; j <= M; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken()) == 1;
+                    graph[i][j] = Integer.parseInt(st.nextToken()) == 1;
                 }
             }
-            
-            // 2. dfs 수행
-            int answer = 0;
+
+            // dfs호출
+            answer = 0;
             for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= M ; j++) {
-                    if(map[i][j] && !visited[i][j]) {
+                for (int j = 1; j <= M; j++) {
+                    if(graph[i][j]) {
                         dfs(i, j);
                         answer++;
                     }
                 }
             }
-            
-            // 3. 출력
+
             bw.write(String.valueOf(answer));
             bw.newLine();
         }
